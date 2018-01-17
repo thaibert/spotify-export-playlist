45998f5cd0f4210e73f8588fa2db76a58aa6cb6e
thaibert <thaibert2@gmail.com>
2018-01-17 11:39:55 +0100

    Add basic functionality for retrieving a response from the Spotify Web API with defualt limits on the amount of tracks

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
+package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	authToken := ""
+	user := ""
+	playlist := ""
+	url := fmt.Sprintf("https://api.spotify.com/v1/users/%s/playlists/%s/tracks", user, playlist)
+
+	// define HTTP client and prepare new request
+	// https://developer.spotify.com/web-api/authorization-guide/
+	client := &http.Client{}
+	req, errMakeRequest := http.NewRequest("GET", url, nil)
+	if errMakeRequest != nil {
+		panic(errMakeRequest)
+	}
+
+	authHeader := fmt.Sprintf("Bearer %s", authToken)
+	req.Header.Add("Authorization", authHeader)
+
+	resp, errGetResponse := client.Do(req)
+	if errGetResponse != nil {
+		panic(errGetResponse)
+	}
+	defer resp.Body.Close()
+
+	// declare body as a byte array of the response body
+	body, errReadBody := ioutil.ReadAll(resp.Body)
+	if errReadBody != nil {
+		panic(errReadBody)
+	}
+
+	ioutil.WriteFile("dump.json", body, 0600)
+	fmt.Println("Export successful")
+}
