157a1c1cf57c307d3a221d80c13499786f2d93c6
thaibert <thaibert2@gmail.com>
2018-01-17 12:04:59 +0100

    Move the retrieval of Spotify API response to its own function

--- main.go
+++ main.go
@@ -10,16 +10,24 @@ func main() {
 	authToken := ""
 	user := ""
 	playlist := ""
-	url := fmt.Sprintf("https://api.spotify.com/v1/users/%s/playlists/%s/tracks", user, playlist)
+	offset := 0
 
+	url := fmt.Sprintf("https://api.spotify.com/v1/users/%s/playlists/%s/tracks?limit=100&offset=%v", user, playlist, offset)
+
+	// ioutil.WriteFile("dump.json", body, 0600)
+	fmt.Println("Export successful")
+}
+
+func getAPIEndpoint(url string, authToken string) []byte {
 	// define HTTP client and prepare new request
-	// https://developer.spotify.com/web-api/authorization-guide/
 	client := &http.Client{}
 	req, errMakeRequest := http.NewRequest("GET", url, nil)
 	if errMakeRequest != nil {
 		panic(errMakeRequest)
 	}
 
+	// add authorization header for the Spotify API
+	// https://developer.spotify.com/web-api/authorization-guide/
 	authHeader := fmt.Sprintf("Bearer %s", authToken)
 	req.Header.Add("Authorization", authHeader)
 
@@ -29,12 +37,10 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// declare body as a byte array of the response body
+	// declare a byte array of the response body
 	body, errReadBody := ioutil.ReadAll(resp.Body)
 	if errReadBody != nil {
 		panic(errReadBody)
 	}
-
-	ioutil.WriteFile("dump.json", body, 0600)
-	fmt.Println("Export successful")
+	return body
 }
