7038ec96d0079546ff1dc8abf64636494efdf5c0
thaibert <thaibert2@gmail.com>
2018-01-20 01:43:08 +0100

    Fetch all songs instead of first 100, then write the results to a datestamped file as search terms

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/tidwall/gjson"
 )
 
 func main() {
+	unformattedURL := "https://api.spotify.com/v1/users/%s/playlists/%s/tracks?fields=%s&limit=100&offset=%v"
+	fields := "items(track(name%2Cartists(name)%2Calbum(name)))%2Ctotal" // extract song name, album name, artists only. Also total number of songs
 
-	// var albums []string
-	// var artists []string
-	// var songs []string
+	var albums []string
+	var songs []string
+	var artists [][]string
 
 	// define the necessary info (user, playlistID and auth token) on the command line
 	args := os.Args[1:] // skip the ./main part
@@ -21,48 +24,71 @@ func main() {
 	playlist := args[1]
 	authToken := args[2]
 
-	//
-	//
-	//- getTracks()
-	// tracks.add(newTracks)
-	// if (size == 100) {
-	// 	offset = 100
-	// 	while (size == 100) {
-	// 		getTracks(urlWithOffset)
-	// 		tracks.add(newTracks)
-	// 		offset += 100
-	// 	}
-	// }
-	//
-	//
-
+	// get up to first 100 tracks
 	offset := 0
-	fields := "items(track(name%2Cartists(name)%2Calbum(name)))%2Ctotal" // extract song name, album name, artists only. Also total number of songs
-	url := fmt.Sprintf("https://api.spotify.com/v1/users/%s/playlists/%s/tracks?fields=%s&limit=100&offset=%v", user, playlist, fields, offset)
+	url := fmt.Sprintf(unformattedURL, user, playlist, fields, offset)
+
 	body := getAPIEndpoint(url, authToken)
-
 	json := string(body)
-	// total := gjson.Get(json, "total")
-	currentAmount := int(gjson.Get(json, "items.#").Num)
+	total := int(gjson.Get(json, "total").Num) // disregard ugly conversion :(
+	trackCount := int(gjson.Get(json, "items.#").Num)
 
-	for i := 0; i < currentAmount; i++ {
-		track := gjson.Get(json, fmt.Sprintf("items.%v.track", i)).String()
+	extractSongData(body, &songs, &albums, &artists)
+	offset += trackCount
+	// if totalTracks > 100, loop through the playlist with offsets increasing by 100 until end is reached
+	for offset < total {
+		url := fmt.Sprintf(unformattedURL, user, playlist, fields, offset)
+		body := getAPIEndpoint(url, authToken)
+		json := string(body)
+		extractSongData(body, &songs, &albums, &artists)
 
-		album := gjson.Get(track, "album.name").Str
-		songName := gjson.Get(track, "name").Str
-
-		var artists []string
-		artistArray := gjson.Get(track, "artists")
-		for artistIndex := 0; artistIndex < len(artistArray.Array()); artistIndex++ {
-			currentArtist := gjson.Get(artistArray.String(), fmt.Sprintf("%v.name", artistIndex))
-			artists = append(artists, currentArtist.String())
-		}
-		fmt.Printf("%s   -   %s     in    %s \n", artists, songName, album)
+		trackCount = int(gjson.Get(json, "items.#").Num)
+		offset += trackCount
 
 	}
-	// tracks = append(tracksForOutput, items...)
+
+	// prepare output file with search terms
+	nowTime := time.Now().String()
+	filename := fmt.Sprintf("%s  %s.txt", nowTime, playlist)
+	file, errCreateFile := os.Create(filename)
+	if errCreateFile != nil {
+		panic(errCreateFile)
+	}
+	defer file.Close()
+
+	for i := 0; i < total; i++ {
+		var artist string
+		for aIndex := 0; aIndex < len(artists[i]); aIndex++ {
+			artist = artist + " " + artists[i][aIndex]
+		}
+		song := songs[i]
+
+		searchTerm := fmt.Sprintf("%s %s\n", artist, song)
+		fmt.Fprintf(file, searchTerm)
+	}
 
 	fmt.Println("Export successful")
+}
+
+func extractSongData(body []byte, songArray *[]string, albumArray *[]string, artistArray *[][]string) {
+	json := string(body)
+
+	trackCount := int(gjson.Get(json, "items.#").Num)
+	for i := 0; i < trackCount; i++ {
+		trackObject := gjson.Get(json, fmt.Sprintf("items.%v.track", i)).String()
+		album := gjson.Get(trackObject, "album.name").Str
+		songName := gjson.Get(trackObject, "name").Str
+
+		var artistOutput []string
+		artistsFromJSON := gjson.Get(trackObject, "artists")
+		for index := 0; index < len(artistsFromJSON.Array()); index++ {
+			currentArtist := gjson.Get(artistsFromJSON.String(), fmt.Sprintf("%v.name", index))
+			artistOutput = append(artistOutput, currentArtist.String())
+		}
+		*songArray = append(*songArray, songName)
+		*albumArray = append(*albumArray, album)
+		*artistArray = append(*artistArray, artistOutput)
+	}
 }
 
 func getAPIEndpoint(url string, authToken string) []byte {
